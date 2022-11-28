2e993eb25bf7d5edaead4f2210cd290e6194b667
Semih <50242428+semihorhanbio@users.noreply.github.com>
2022-11-28 18:02:02 +0000

    add edit feauture

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 todos = []
 
 while True:
-  user_action = input("Type add, show or exit:")
+  user_action = input("Type add, show, edit or exit: ")
   user_action = user_action.strip()
 
   match user_action:
     case "add":
-      todo = input("Add a todo:")
+      todo = input("Add a todo: ")
       todos.append(todo)
     case "show":
       print("\n".join(todos))
+    case "edit":
+      number = int(input("Number of the todo to edit: "))
+       #Adjust for zero indexing
+      number -= 1
+      new_todo = input("Enter new todo: ")
+      # Change todo with new one
+      todos[number] =  new_todo
     case "exit":
       break
 

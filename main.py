d1f3a221275664659208bc301edb832ae830e6c7
Semih <50242428+semihorhanbio@users.noreply.github.com>
2022-12-03 14:35:16 +0000

    add a complete feature

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-  user_action = input("Type add, show, edit or exit: ")
+  user_action = input("Type add, show, edit, complete or exit: ")
   user_action = user_action.strip()
 
   match user_action:
@@ -11,7 +11,7 @@ while True:
     
     case "show":
       for index, item in enumerate(todos):
-        print(f"{index}-{item}")
+        print(f"{index + 1}-{item}")
     
     case "edit":
       number = int(input("Number of the todo to edit: "))
@@ -20,7 +20,11 @@ while True:
       new_todo = input("Enter new todo: ")
       # Change todo with new one
       todos[number] =  new_todo
-    
+
+    case "complete":
+      number = int(input("Number of the todo to complete: "))
+      todos.pop(number - 1)
+      
     case "exit":
       break
 
